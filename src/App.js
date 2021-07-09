8b12ba84932b5b4109096c3c82f9cc37b4b8ff39
Necrobone <necrobone@hotmail.com>
2021-07-09 13:38:08 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+import ExpenseList from './components/Expenses/ExpenseList';
+
+const App = () => {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseList expenses={expenses} />
+        </div>
+    );
 }
 
 export default App;
