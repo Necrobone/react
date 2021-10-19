1bf7e2971b29e0598c8cfbef3dc6807fa1e3ba08
Necrobone <necrobone@hotmail.com>
2021-10-19 23:04:37 +0200

    Creating components and basic UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import Header from './components/Layout/Header/Header';
+import { Fragment, useState } from 'react';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    }
+
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    }
+
+    return (
+        <Fragment>
+            {cartIsShown && <Cart onClose={hideCartHandler} />}
+            <Header onShownCart={showCartHandler} />
+            <main>
+                <Meals />
+            </main>
+        </Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import MealsSummary from './MealsSummary/MealsSummary';
+import AvailableMeals from './AvailableMeals/AvailableMeals';
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    );
+};
+
+export default Meals;
