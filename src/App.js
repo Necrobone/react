be8452476bf6e309d9cf5c5684d8e2549d0350b5
Necrobone <necrobone@hotmail.com>
2022-01-07 20:23:00 +0100

    Error handling

--- src/App.js
+++ src/App.js
@@ -6,22 +6,52 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     async function fetchMoviesHandler() {
         setIsLoading(true);
-        const response = await fetch('https://swapi.dev/api/films');
-        const data = await response.json();
+        setError(null);
 
-        const transformedMovies = data.results.map(movie => {
-            return {
-                id: movie.episode_id,
-                title: movie.title,
-                openingText: movie.opening_crawl,
-                releaseDate: movie.release_date,
-            };
-        });
-        setMovies(transformedMovies);
+        try {
+            const response = await fetch('https://swapi.dev/api/films');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const data = await response.json();
+
+            const transformedMovies = data.results.map(movie => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                };
+            });
+            setMovies(transformedMovies);
+        } catch (responseError) {
+            setError(responseError.message);
+        }
+
         setIsLoading(false);
+    }
+
+    let content;
+
+    switch (true) {
+        case movies.length > 0:
+            content = <MoviesList movies={movies} />;
+            break;
+        case error:
+            content = <p>{error}</p>;
+            break;
+        case isLoading:
+            content = <p>Loading</p>;
+            break;
+        default:
+            content = <p>Found no movies.</p>;
+            break;
     }
 
     return (
@@ -29,11 +59,7 @@ function App() {
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
-            <section>
-                {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-                {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-                {isLoading && <p>Loading...</p>}
-            </section>
+            <section>{content}</section>
         </React.Fragment>
     );
 }
