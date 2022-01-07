24b53f2df15a090e0a78055d1fbe3e7436a4df0f
Necrobone <necrobone@hotmail.com>
2022-01-07 22:21:30 +0100

    Removing dependencies

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import useHttp from './hooks/use-http';
 function App() {
     const [tasks, setTasks] = useState([]);
 
-    const transformTasks = tasksObj => {
-        const loadedTasks = [];
-
-        for (const taskKey in tasksObj) {
-            loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-        }
-
-        setTasks(loadedTasks);
-    };
-
-    const {isLoading, error, sendRequest: fetchTasks} = useHttp(
-        { url: 'https://react-http-95324-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'},
-        transformTasks
-    );
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        const transformTasks = tasksObj => {
+            const loadedTasks = [];
+
+            for (const taskKey in tasksObj) {
+                loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+            }
+
+            setTasks(loadedTasks);
+        };
+
+        fetchTasks(
+            { url: 'https://react-http-95324-default-rtdb.europe-west1.firebasedatabase.app/tasks.json' },
+            transformTasks,
+        );
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
