b78c329824e27736fa8f8be6aafebc650f0909e3
Necrobone <necrobone@hotmail.com>
2022-01-08 17:56:38 +0100

    Practice with an empty form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import SimpleInput from './components/SimpleInput';
+import BasicForm from './components/BasicForm';
 
 function App() {
-  return (
-    <div className="app">
-      <SimpleInput />
-    </div>
-  );
+    return (
+        <div className="app">
+            <BasicForm />
+        </div>
+    );
 }
 
 export default App;
