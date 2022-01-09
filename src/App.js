09dc67b24a20e909b47567623b439c103a985538
Necrobone <necrobone@hotmail.com>
2022-01-09 16:48:10 +0100

    Redux async in components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector(state => state.cart);
+    const notification = useSelector(state => state.ui.notification);
+
+    useEffect(async () => {
+        const sendCartData = async () => {
+            dispatch(uiActions.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending cart data!',
+            }));
+            const response = await fetch('https://react-http-95324-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.');
+            }
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Cart data sent successfully!',
+            }));
+        };
+
+        if (isInitial) {
+            isInitial = false;
+            return;
+        }
+
+        sendCartData().catch(error => {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!',
+            }));
+        });
+    }, [cart, dispatch]);
 
     return (
-        <Layout>
-            {showCart && <Cart />}
-            <Products />
-        </Layout>
+        <Fragment>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
     );
 }
 
