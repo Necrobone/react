6f1eb4a3834b26b527000e17230a481d5c7dd88d
Necrobone <necrobone@hotmail.com>
2022-01-09 19:52:02 +0100

    New project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Products from './components/Products';
-import MainHeader from './components/MainHeader';
-import ProductDetail from './components/ProductDetail';
-
 function App() {
-    return (
-        <div>
-            <MainHeader />
-            <main>
-                <Routes>
-                    <Route path="/" exact element={<Navigate to="/welcome" />} />
-                    <Route path="/welcome" element={<Welcome />} />
-                    <Route path="/products" element={<Products />} />
-                    <Route path="/products/:productId" element={<ProductDetail />} />
-                </Routes>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+
+    </div>
+  );
 }
 
 export default App;
