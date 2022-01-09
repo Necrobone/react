3b3282bc892b240eb3d08a094fdff0f1e2addf9b
Necrobone <necrobone@hotmail.com>
2022-01-09 18:43:47 +0100

    Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Products from './components/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
     return (
         <div>
-            <Routes>
-                <Route path="/welcome" element={<Welcome />} />
-                <Route path="/products" element={<Products />} />
-            </Routes>
+            <MainHeader />
+            <main>
+                <Routes>
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/products" element={<Products />} />
+                </Routes>
+            </main>
         </div>
     );
 }
