87fb83f22f355ae3719b396a2341871d34ae85ef
Necrobone <necrobone@hotmail.com>
2022-01-09 21:24:11 +0100

    Dummy data and layout wrapper

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,18 @@
+import QuoteList from '../components/quotes/QuoteList';
+import { Fragment } from 'react';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is Fun!' },
+    { id: 'q2', author: 'Necro', text: 'Learning React is Crap!' },
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return (
+        <Fragment>
+            <h1>All Quotes Page</h1>
+            <QuoteList quotes={DUMMY_QUOTES}/>
+        </Fragment>
+    );
 };
 
 export default AllQuotes;
