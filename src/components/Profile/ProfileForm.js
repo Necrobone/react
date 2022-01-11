457beb7b2b4463e7957bf792c107ae607cb6b5d6
Necrobone <necrobone@hotmail.com>
2022-01-11 22:15:56 +0100

    Use token for request

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,41 @@
 import classes from './ProfileForm.module.css';
+import { useContext, useRef } from 'react';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+    const newPasswordInputRef = useRef();
+    const context = useContext(AuthContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredPassword = newPasswordInputRef.current.value;
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBglMxIcjMGe9UkLKxW2en_sBt54OxCFxY', {
+            body: JSON.stringify({
+                idToken: context.token,
+                password: enteredPassword,
+                returnSecureToken: true,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(res => {
+
+        });
+    };
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor="new-password">New Password</label>
+                <input type="password" id="new-password" ref={newPasswordInputRef} minLength="7" />
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
+};
 
 export default ProfileForm;
