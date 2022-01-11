d401c7abc93dc0750e37b8c0e89a2719262bed28
Necrobone <necrobone@hotmail.com>
2022-01-11 22:35:16 +0100

    Protecting pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+    const context = useContext(AuthContext);
+
     return (
         <Layout>
             <Switch>
                 <Route path="/" exact>
                     <HomePage />
                 </Route>
-                <Route path="/auth">
-                    <AuthPage />
-                </Route>
+                {!context.isLoggedIn && (
+                    <Route path="/auth">
+                        <AuthPage />
+                    </Route>
+                )}
                 <Route path="/profile">
-                    <UserProfile />
+                    {context.isLoggedIn && <UserProfile />}
+                    {!context.isLoggedIn && <Redirect to='/auth' />}
+                </Route>
+                <Route path="*">
+                    <Redirect to="/" />
                 </Route>
             </Switch>
         </Layout>
